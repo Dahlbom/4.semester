41ba537967b5c9102d9caf439ea7a44d548fd0a2
Frederik Dahlbom <au618089@uni.au.dk>
2020-02-08 09:33:17 +0100

    small changes to MainWindows.xaml

--- GUI/GUI_Ex_2_1/GUI_Ex_2_1/MainWindow.xaml.cs
+++ GUI/GUI_Ex_2_1/GUI_Ex_2_1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GUI_Ex_3
+namespace GUI_Ex_2_1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
