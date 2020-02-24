89484060c45bbc0c15b9d1ef2fd9a478dc8abfb4
Frederik Dahlbom <au618089@uni.au.dk>
2020-02-24 10:08:46 +0100

    Oploading for LAB 8

---
+++ GUI/GUI_LAB_9/Delopgave_2/AgentIdToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AgentAssignment
+{
+    public class AgentIdToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(targetType == typeof(Brush));
+            string id = value as string;
+            if (id == null)
+                id = "";
+            return (id == "007" ? Brushes.Blue : Brushes.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
