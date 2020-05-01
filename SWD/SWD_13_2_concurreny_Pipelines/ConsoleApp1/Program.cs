5b90b4b6a2ea2cae9fc14bad4103481e10eb99fc
Frederik Dahlbom <dahlbom16@gmail.com>
2020-05-01 11:57:55 +0200

    Started the SWD lab exercise concurrency pipeline

---
+++ SWD/SWD_13_2_concurreny_Pipelines/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Pipelines;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SequentialStringCompression SSC = new SequentialStringCompression("ABC", 100000, 25000);
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            
+            double totaltime = SSC.Run();
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"The time it took was: {stopwatch.ElapsedMilliseconds}");
+
+        }
+    }
+}
